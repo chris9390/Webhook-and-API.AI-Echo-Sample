01b4530bf3d769c07b1d030e05556121e0eb022f
chris9390 <chris9390@naver.com>
2018-07-17 17:53:37 +0900

    Update index.js

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(bodyParser.json());
 
+app.set('port', (process.env.PORT || 5000));
+
 /*
 app.get('/', function (req, res) {
     res.send("hi!")
