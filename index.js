a497a6def7bf4b6c7011fa52c0495134b19720a7
chris9390 <chris9390@naver.com>
2018-07-17 16:02:02 +0900

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.get('/', function (req, res) {
     res.send("hi!")
 });
 
-app.get("/weather", function(req, res) {
+app.get("/", function(req, res) {
 
   var appKey = '1b4e36fb-2bb9-4380-8930-a63b1bfcefef';
   var lat_value = 40.1234;
