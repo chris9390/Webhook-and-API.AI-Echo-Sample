615570a0bab8b70494b4f2d60e1fe95b0298f2d9
chris9390 <chris9390@naver.com>
2018-07-17 13:15:42 +0900

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ restService.post("/echo", function(req, res) {
     //speech: speech,
     //displayText: speech,
     //source: "webhook-echo-sample"
-    fulfillmentText: "what?",
+    fulfillmentText: speech,
     source: "EchoService"
   });
 });
